6ff0607444be328608915f309b78ebb0df76738f
Irving Jose Duran <ijd.irving@gmail.com>
2025-06-05 21:46:25 -0400

    finish minimum

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
-import UsersTable from './components/UsersTable.js'
+import UsersTable from "./components/UsersTable.js";
+import AddUserForm from "./components/AddNewUserForm.js";
 
 function App() {
   const [allUsers, getAllUsers] = useState([]);
-  const [addUserForm, setAddUserForm] = useState({ name: "", zipCode: "" });
-  const [updateUserForm, setUpdateUserForm] = useState(null);
   const [message, setMessage] = useState("");
   const [submitting, setSubmitting] = useState(false);
 
@@ -31,122 +30,11 @@ function App() {
       });
   }, [submitting]);
 
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    setAddUserForm((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleOnUpdate = (e) => {
-    const { name, value } = e.target;
-    setUpdateUserForm((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-    axios
-      .post("http://localhost:8080/users", addUserForm)
-      .then((response) => {
-        console.log("User created:", response.data);
-        setAddUserForm({ name: "", zipCode: "" });
-        setSubmitting(false);
-      })
-      .catch((error) => {
-        console.error("Error creating user:", error);
-      });
-  };
-
-  const handleUpdate = (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-    const { id, name, zipCode } = updateUserForm;
-    if ((id && name) || zipCode) {
-      axios
-        .put(`http://localhost:8080/users/${updateUserForm.id}`, {
-          name: name,
-          zipCode: zipCode,
-        })
-        .catch((error) => {
-          console.error("Error creating user:", error);
-        });
-    }
-  };
-
-  const handleDelete = (id) => {
-    setSubmitting(true);
-    axios
-      .delete(`http://localhost:8080/users/${id}`)
-      .then((res) => {
-        console.log(res.data.message);
-        setSubmitting(false);
-      })
-      .catch((err) => {
-        console.error("Error deleting user:", err);
-      });
-  };
-
   return (
-    <div className="App App-header">
+    <div className="App">
       <header className="">{message} </header>
-      
-      <form onSubmit={handleSubmit}>
-        <input name="name" value={addUserForm.name} onChange={handleOnChange} />
-        <input
-          name="zipCode"
-          value={addUserForm.zipCode}
-          onChange={handleOnChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <UsersTable users={allUsers} setSubmitting={setSubmitting}/>
-      {/* <ui>
-        {allUsers.length &&
-          allUsers.map(({ name, id, lat, lon, timezone, zipCode }) => {
-            return (
-              <li id={id}>
-                {updateUserForm?.id === id ? (
-                  <form onSubmit={handleUpdate}>
-                    <input
-                      name="name"
-                      value={updateUserForm.name}
-                      onChange={handleOnUpdate}
-                    />
-                    <input
-                      name="zipCode"
-                      value={updateUserForm.zipCode}
-                      onChange={handleOnUpdate}
-                    />
-                    <button type="submit">Update</button>
-                    <button onClick={() => setUpdateUserForm(null)}>
-                      cancel
-                    </button>
-                  </form>
-                ) : (
-                  <div>
-                    {name}
-                    {lat}
-                    {lon}
-                    {timezone}
-                    {zipCode}
-                    <button
-                      // style={{ background: "black" }}
-                      onClick={() => setUpdateUserForm({ id })}
-                    >
-                      Edit
-                    </button>
-                    <button onClick={() => handleDelete(id)}>delete</button>
-                  </div>
-                )}
-              </li>
-            );
-          })}
-      </ui> */}
+      <AddUserForm setSubmitting={setSubmitting} />
+      <UsersTable users={allUsers} setSubmitting={setSubmitting} />
     </div>
   );
 }
---
+++ client/src/components/AddNewUserForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+
+export default function AddNewUserForm({ setSubmitting }) {
+  const [addUserForm, setAddUserForm] = useState({ name: "", zipCode: "" });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setAddUserForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    axios
+      .post("http://localhost:8080/users", addUserForm)
+      .then((response) => {
+        console.log("User created:", response.data);
+        setAddUserForm({ name: "", zipCode: "" });
+        setSubmitting(false);
+      })
+      .catch((error) => {
+        console.error("Error creating user:", error);
+      });
+  };
+  return (
+    <Box
+      component="form"
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
+      noValidate
+      autoComplete="off"
+    >
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          name="name"
+          placeholder="Name"
+          value={addUserForm.name}
+          onChange={handleOnChange}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          name="zipCode"
+          placeholder="Zip Code"
+          value={addUserForm.zipCode}
+          onChange={handleOnChange}
+        />
+      </div>
+
+      <Button
+        variant="contained"
+        endIcon={<SendIcon />}
+        onClick={handleSubmit}
+        style={{ alignSelf: "center" }}
+      >
+        Add User
+      </Button>
+    </Box>
+  );
+}
